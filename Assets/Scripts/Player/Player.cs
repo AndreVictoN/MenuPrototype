e4b9b20a52784d9e58811f925201a08dc8c00858
André Victor Gonçalves Nascimento <andrevictor20011@gmail.com>
2024-05-28 15:26:06 -0300

    Added new assets and animations to the player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -12,6 +13,11 @@ public class Player : MonoBehaviour
     public float runningSpeed =  20;
 
     public float jumpForce = 20;
+
+    public string boolRun = "run";
+    public string boolRunning = "running";
+
+    public Animator animator;
 
     private float _currentVelocity;
 
@@ -39,25 +45,45 @@ public class Player : MonoBehaviour
         if(Input.GetKey(KeyCode.LeftShift))
         {
             _currentVelocity = runningSpeed;
+
+            animator.SetBool(boolRunning, true);
         }
         else
         {
             _currentVelocity = velocity;
+
+            animator.SetBool(boolRunning, false);
         }
 
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             myRigidBody.velocity = new Vector2(-1 * _currentVelocity, myRigidBody.velocity.y);
 
-            gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+            //gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+
+            if(myRigidBody.transform.localScale.x != -1)
+            {
+                myRigidBody.transform.DOScaleX(-1, .1f);
+            }
+
+            animator.SetBool(boolRun, true);
         }else if(Input.GetKey(KeyCode.RightArrow))
         {
             myRigidBody.velocity = new Vector2(1 * _currentVelocity, myRigidBody.velocity.y);
 
-            gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            //gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            if(myRigidBody.transform.localScale.x != 1)
+            {
+                myRigidBody.transform.DOScaleX(1, .1f);
+            }
+
+            animator.SetBool(boolRun, true);
         }else
         {
             myRigidBody.velocity = new Vector2(0 * _currentVelocity, myRigidBody.velocity.y);
+
+            animator.SetBool(boolRun, false);
         }
     }
 
